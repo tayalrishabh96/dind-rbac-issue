6fc62a9899426e778b9c4e8af2f946830e3e023a
Rishabh Tayal <135199635+tayalrishabh96@users.noreply.github.com>
2026-06-23 13:12:21 +0530

    dummy commit 4

--- debug.sh
+++ debug.sh
@@ -3,5 +3,5 @@ echo "Listing all pods across all namespaces:"
 kubectl get po -A
 echo "Listing all services across all namespaces:"
 kubectl get svc -A
-echo "Listing all deployments across all namespaces :"
+echo "Listing all deployments across all namespaces:"
 kubectl get deploy -A
